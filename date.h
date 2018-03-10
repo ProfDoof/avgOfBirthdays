3c6e9956aa9be8a2b5e1559c24041d8e2e778d1c
John Michael Marsden <jmm15f@acu.edu>
2018-03-10 09:22:20 -0600

    Major Update

--- date.h
+++ date.h
@@ -1,3 +1,8 @@
+#ifndef DATE_H
+#define DATE_H
+
+using namespace std;
+
 
 class Date
 {
@@ -5,25 +10,39 @@ class Date
 		int year;
 		int month;
 		int day;
-		int[12] days_in_month = {31,28,31,30,31,30,31,31,30,31,30,31}
+		int days_in_month[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 	
 	public:
 		//constructor
 		Date(int y, int m, int d) : year(y), month(m), day(d) {}
 
 		//compare dates
-		bool operator< (Date a, Date b)
+		const bool operator< (Date b) const
 		{
-			int aDate = (a.getYear() * 10000)+(a.getMonth*100)+(a.getDay);
-			int bDate = (b.getYear() * 10000)+(b.getMonth*100)+(b.getDay);
+			int aDate = (this->getYear() * 10000)+(this->getMonth()*100)+(this->getDay());
+			int bDate = (b.getYear() * 10000)+(b.getMonth()*100)+(b.getDay());
 			return aDate < bDate;
+		}
+
+		const bool operator== (Date b) const
+		{
+			int aDate = (this->getYear() * 10000)+(this->getMonth()*100)+(this->getDay());
+			int bDate = (b.getYear() * 10000)+(b.getMonth()*100)+(b.getDay());
+			return aDate == bDate;
+		}
+
+		const bool operator> (Date b) const 
+		{
+			int aDate = (this->getYear() * 10000)+(this->getMonth()*100)+(this->getDay());
+			int bDate = (b.getYear() * 10000)+(b.getMonth()*100)+(b.getDay());
+			return aDate > bDate;
 		}
 		
 		//convert to a number based on base year and current year
-		int dateToNum(Date a, int base_year)
+		const int dateToNum(int base_year) const
 		{
-			num_years = a.getYear() - base_year;
-			num_days = 0;
+			int num_years = this->getYear() - base_year;
+			int num_days = 0;
 			for(int i = 0; i < num_years; i++)
 			{
 				if ( (base_year+i%4) == 0 && ((base_year+i%100) != 0  || (base_year+i%400) == 0 ))
@@ -32,27 +51,54 @@ class Date
 					num_days += 365;
 			}
 			
-			for(int i = 0; i < a.getMonth(); i++)
+			for(int i = 0; i < this->getMonth()-1; i++)
 			{
 				num_days += days_in_month[i];
-				if ( i == 2 && (a.getYear()%4) == 0 && ((a.getYear()%100) != 0  || (a.getYear()%400) == 0 ))
+				if ( i == 1 && (this->getYear()%4) == 0 && ((this->getYear()%100) != 0  || (this->getYear()%400) == 0 ))
 					num_days += 1;
 			}
 		
-			num_days += a.getDay();
+			num_days += this->getDay();
 			
 			return num_days;
 		}
 		
 		//convert a number to a date
-		
+		void numToDate(int a, int base_year)
+		{
+			int i;
+			int num_days = a;
+			//remove the years from the num in order to find the number of years in it.
+			for(i = 0; (num_days > 365) || (num_days > 366); i++)
+			{
+				cout << a << endl;
+				if ( (base_year+i%4) == 0 && ((base_year+i%100) != 0  || (base_year+i%400) == 0 ))
+					num_days -= 366;
+				else
+					num_days -= 365;
+			}
+			this->setYear(base_year+i);
+
+			for(i = 0; (i == 1 && num_days > 28 ) || ((i == 0 || i == 2 || i == 4 || i == 6 || i == 7 || i == 9 || i == 11) && num_days > 31) || ((i == 3 || i == 5 || i == 8 || i == 10) && num_days > 30); i++)
+			{
+				cout << num_days << endl;
+				num_days -= days_in_month[i];
+				if ( i == 1 && (this->getYear()%4) == 0 && ((this->getYear()%100) != 0  || (this->getYear()%400) == 0 ))
+					num_days -= 1;
+			}
+			this->setMonth(i+1);
+			this->setDay(num_days);
+		}
 		
 		//setters and getters
 		void setYear(int tts) {year = tts;}
 		void setMonth(int tts) {month = tts;}
 		void setDay(int tts) {day = tts;}
 		
-		int getYear() {return year;}
-		int getMonth() {return month;}
-		int getDay() {return day;}
-}
+		int getYear() const {return year;}
+		int getMonth() const {return month;}
+		int getDay() const {return day;}
+};
+
+
+#endif
